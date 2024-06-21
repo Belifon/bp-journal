b2b3c7f423f2da84e00de3a1dd36ebae60d9e149
Tomic <dryelissa@gmail.com>
2024-06-21 11:22:35 +0300

    Working on apology branch, currently working fine, will go through json process now

--- helpers.py
+++ helpers.py
@@ -11,14 +11,19 @@ from functools import wraps
 
 
 def apology(message, code=400):
-    
+
+
+    # Print message and code to check if the function is called correctly TO BE DELETED
+    print(f"Apology called with message: {message} and code: {code}")
+
     # Render message as an apology to user, imported from CS50x Finance
     def escape(s):
-        
+
         # Escape special characters. https://github.com/jacebrowning/memegen#special-characters
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"), ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
         return s
+    
     
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
